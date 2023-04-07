def970d941bc674eabe3a74292aa4aa60c083747
Ajax Lima <ajaxlima94@gmail.com>
2023-04-07 13:26:41 -0300

    Style: marcação da pag atual no header
    Ajuste para sempre pegar o último elemento da url para efetuar a devida marcação no header, de qual pag o usuário está

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ window.onload = () => {
             index: "home",
             faq: "faq"
         }
-        const actualPage = document.head.baseURI.split('/')[3]?.split('.')[0];
+        const actualPage = document.head.baseURI.split('/').pop()?.split('.')[0];
         const element = document.querySelector(`[${pages[actualPage || "index"]}]`);
         element?.setAttribute("style", "border-bottom: 0.2rem solid #000;");
     }, 500)
