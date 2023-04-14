741a0b21870fefe573133e9ee302ffde5590b3f7
Ajax Lima <ajaxlima94@gmail.com>
2023-04-14 16:56:06 -0300

    Feat: Add menu mobile

--- js/getHeaderAndFooter.js
+++ js/getHeaderAndFooter.js
@@ -8,6 +8,9 @@ async function getHeader() {
         .then(res => res.text())
         .catch(err => err.message);
     document.getElementsByTagName('header')[0].innerHTML = header;
+    document.getElementsByClassName('hamburger')[0].addEventListener('click', showHideMenu);
+    const menu_mobile = document.getElementsByClassName('menu-mobile')[0];
+    menu_mobile.addEventListener('click', hideMenu);
 }
 
 async function getFooter() {
@@ -15,4 +18,31 @@ async function getFooter() {
         .then(res => res.text())
         .catch(err => err.message);
     document.getElementsByTagName('footer')[0].innerHTML = footer;
+}
+
+function showHideMenu() {
+    const menu = document.getElementsByClassName('menu-mobile')[0];
+    if (menu?.style.display == '' || menu?.style.display == 'none') {
+        showMenu();
+    } else {
+        hideMenu();
+    }
+}
+
+function hideMenu() {
+    const menu = document.getElementsByClassName('menu-mobile')[0];
+    const hamburger = document.getElementsByClassName('hamburger')[0];
+
+    menu?.style.setProperty("display", "none");
+    hamburger?.style.setProperty("transform", "rotate(0deg)");
+    document.querySelector(".header>:last-child")?.style.setProperty("display", "none");
+}
+
+function showMenu() {
+    const menu = document.getElementsByClassName('menu-mobile')[0];
+    const hamburger = document.getElementsByClassName('hamburger')[0];
+
+    menu?.style.setProperty('display', 'flex');
+    document.querySelector(".header>:last-child")?.style.setProperty("display", "block");
+    hamburger?.style.setProperty("transform", "rotate(-90deg)");
 }
