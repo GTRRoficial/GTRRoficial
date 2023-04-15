1437ea9761f45e1e4b1b6373baafe569ab8f6cbe
Ajax Lima <ajaxlima94@gmail.com>
2023-04-15 11:23:11 -0300

    Style: ajuste para mobile e adição de uma animação

--- js/home.js
+++ js/home.js
@@ -1,23 +1,24 @@
 (function () {
     const asks = document.getElementsByClassName('ask-container');
     for (let i = 0; i < asks.length; i++) {
-        const title = asks[i].querySelector("h2");
+        const title = asks[i].querySelector(".panel-head");
         const resp = asks[i].querySelector("p");
-        const sign = title?.querySelector(".sign");
+        const plus = title?.querySelector(".sign")?.querySelector(':first-child');
+        const minus = title?.querySelector(".sign")?.querySelector(':last-child');
         title?.addEventListener('click', () => {
             if (resp?.style.display == '' || resp?.style.display == 'none') {
                 resp?.style.setProperty('display', 'block');
-                resp?.style.setProperty("animation", "fadeIn 1s");
                 setTimeout(() => {
                     resp?.style.setProperty("transform", "scale(1)");
+                    minus?.style.setProperty('display', 'block');
+                    plus?.style.setProperty('display', 'none');
                 }, 0)
-                sign.innerHTML = '<img class="fade-in-image" src="./assets/icons/minus.png"></img>';
             } else {
                 resp?.style.setProperty("transform", "scale(1)");
-                resp?.style.setProperty("animation", "fadeIn 1s");
-                sign.innerHTML = '<img class="fade-in-image" src="./assets/icons/plus.png"></img>';
                 setTimeout(() => {
                     resp?.style.setProperty('display', 'none');
+                    minus?.style.setProperty('display', 'none');
+                    plus?.style.setProperty('display', 'block');
                 }, 100)
             }
         })
